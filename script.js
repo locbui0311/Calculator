d6f862cf25fc806fc4a41cc32a1557087395c1ac
locbui0311 <locbui.it.0311@gmail.com>
2022-03-05 17:13:21 +0700

    code file scipt.js

---
+++ script.js
@@ -0,0 +1,63 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear() {
+        this.currentOperand= ''
+        this.previousOperand=''
+        this.operation=undefined
+    }
+    delete(){
+
+    }
+    //////put number show calculator
+    appendNumber(){
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+    //////chosse calculation show 
+    chooseOperation(operation){
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+
+    compute(){
+
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector(['data-equals'])
+const deleteButton = document.querySelector(['data-delete'])
+const allClearButton = document.querySelector(['data-all-clear'])
+const previousOperandTextElement = document.querySelector(['data-previous-operand'])
+const currentOperandTextElement = document.querySelector(['data-current-operand'])
+
+const calculator=new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+//////update calculation to current
+numberButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+numberButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+
+
+
